35bdba6b15cea148bd7ffacc1b9a81c26a3f8553
Lio Mikael <lmybanez@ndkc.edu.ph>
2026-09-06 00:01:01 +0800

    Added temporary debug line

--- app/database.py
+++ app/database.py
@@ -5,10 +5,14 @@ from sqlmodel import create_engine, SQLModel, Session
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./dev.db")
+print("Using DATABASE_URL:", DATABASE_URL)  # temporary debug line
+
 engine = create_engine(DATABASE_URL, echo=True)
+
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
+
 
 def get_session():
     with Session(engine) as session:
