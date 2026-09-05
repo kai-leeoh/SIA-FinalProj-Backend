6aefa7025d103f17beed83ab6685db21cf816929
Lio Mikael <lmybanez@ndkc.edu.ph>
2026-09-05 16:59:43 +0800

    Migrate to Neon Postgres

--- app/database.py
+++ app/database.py
@@ -1,7 +1,11 @@
+import os
+from dotenv import load_dotenv
 from sqlmodel import create_engine, SQLModel, Session
 
-sqlite_url = "sqlite:///./dev.db"
-engine = create_engine(sqlite_url, echo=True)
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./dev.db")
+engine = create_engine(DATABASE_URL, echo=True)
 
 def create_db_and_tables():
     SQLModel.metadata.create_all(engine)
